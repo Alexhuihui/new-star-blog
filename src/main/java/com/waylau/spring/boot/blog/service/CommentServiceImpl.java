f3c83de1420adc42ecc357741d5123ce3f13377f
alex <2930807240>
2019-01-22 09:06:49 +0800

    评论管理

--- src/main/java/com/waylau/spring/boot/blog/service/CommentServiceImpl.java
+++ src/main/java/com/waylau/spring/boot/blog/service/CommentServiceImpl.java
@@ -10,26 +10,22 @@ import com.waylau.spring.boot.blog.repository.CommentRepository;
 
 /**
  * Comment 服务.
- * 
- * @since 1.0.0 2017年4月9日
- * @author <a href="https://waylau.com">Way Lau</a>
  */
 @Service
 public class CommentServiceImpl implements CommentService {
 
-	@Autowired
-	private CommentRepository commentRepository;
-	/* (non-Javadoc)
-	 * @see com.waylau.spring.boot.blog.service.CommentService#removeComment(java.lang.Long)
-	 */
-	@Override
-	@Transactional
-	public void removeComment(Long id) {
-		commentRepository.delete(id);
-	}
-	@Override
-	public Comment getCommentById(Long id) {
-		return commentRepository.findOne(id);
-	}
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Override
+    @Transactional
+    public void removeComment(Long id) {
+        commentRepository.deleteById(id);
+    }
+
+    @Override
+    public Comment getCommentById(Long id) {
+        return commentRepository.getOne(id);
+    }
 
 }
