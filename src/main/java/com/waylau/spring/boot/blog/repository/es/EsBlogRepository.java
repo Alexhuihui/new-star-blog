a0e3f425ede9e8d12c1acb1a2e4568c5d20ef12c
alex <2930807240>
2019-01-24 00:10:30 +0800

    搜索管理

--- src/main/java/com/waylau/spring/boot/blog/repository/es/EsBlogRepository.java
+++ src/main/java/com/waylau/spring/boot/blog/repository/es/EsBlogRepository.java
@@ -8,22 +8,20 @@ import com.waylau.spring.boot.blog.domain.es.EsBlog;
 
 /**
  * Blog 存储库.
- * 
- * @since 1.0.0 2017年3月12日
- * @author <a href="https://waylau.com">Way Lau</a> 
  */
 public interface EsBlogRepository extends ElasticsearchRepository<EsBlog, String> {
- 
-	/**
-	 * 模糊查询(去重)
-	 * @param title
-	 * @param Summary
-	 * @param content
-	 * @param tags
-	 * @param pageable
-	 * @return
-	 */
-	Page<EsBlog> findDistinctEsBlogByTitleContainingOrSummaryContainingOrContentContainingOrTagsContaining(String title,String Summary,String content,String tags,Pageable pageable);
-	
-	EsBlog findByBlogId(Long blogId);
+
+    /**
+     * 模糊查询(去重)
+     *
+     * @param title
+     * @param Summary
+     * @param content
+     * @param tags
+     * @param pageable
+     * @return
+     */
+    Page<EsBlog> findDistinctEsBlogByTitleContainingOrSummaryContainingOrContentContainingOrTagsContaining(String title, String Summary, String content, String tags, Pageable pageable);
+
+    EsBlog findByBlogId(Long blogId);
 }
--- src/main/java/com/waylau/spring/boot/blog/service/EsBlogService.java
+++ src/main/java/com/waylau/spring/boot/blog/service/EsBlogService.java
@@ -13,8 +13,6 @@ import com.waylau.spring.boot.blog.vo.TagVO;
 /**
  * Blog 服务接口.
  * 
- * @since 1.0.0 2017年4月7日
- * @author <a href="https://waylau.com">Way Lau</a>
  */
 public interface EsBlogService {
  	
@@ -27,63 +25,45 @@ public interface EsBlogService {
 	
 	/**
 	 * 更新 EsBlog
-	 * @param EsBlog
-	 * @return
 	 */
 	EsBlog updateEsBlog(EsBlog esBlog);
 	
 	/**
 	 * 根据id获取Blog
-	 * @param id
-	 * @return
 	 */
 	EsBlog getEsBlogByBlogId(Long blogId);
  
 	/**
 	 * 最新博客列表，分页
-	 * @param keyword
-	 * @param pageable
-	 * @return
 	 */
 	Page<EsBlog> listNewestEsBlogs(String keyword, Pageable pageable);
  
 	/**
 	 * 最热博客列表，分页
-	 * @param keyword
-	 * @param pageable
-	 * @return
 	 */
 	Page<EsBlog> listHotestEsBlogs(String keyword, Pageable pageable);
 	
 	/**
 	 * 博客列表，分页
-	 * @param pageable
-	 * @return
 	 */
 	Page<EsBlog> listEsBlogs(Pageable pageable);
 	/**
 	 * 最新前5
-	 * @param keyword
-	 * @return
 	 */
 	List<EsBlog> listTop5NewestEsBlogs();
 	
 	/**
 	 * 最热前5
-	 * @param keyword
-	 * @return
 	 */
 	List<EsBlog> listTop5HotestEsBlogs();
 	
 	/**
 	 * 最热前 30 标签
-	 * @return
 	 */
 	List<TagVO> listTop30Tags();
 
 	/**
 	 * 最热前12用户
-	 * @return
 	 */
 	List<User> listTop12Users();
 }
